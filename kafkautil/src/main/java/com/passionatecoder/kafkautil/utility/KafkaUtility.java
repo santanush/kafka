45b272c1da229ea1aa7273c4a4410e590cc42b41
santanusaha <santanush@rediffmail.com>
2021-05-10 16:35:58 +0530

    Second commit

--- kafkautil/src/main/java/com/passionatecoder/kafkautil/utility/KafkaUtility.java
+++ kafkautil/src/main/java/com/passionatecoder/kafkautil/utility/KafkaUtility.java
@@ -40,7 +40,7 @@ public class KafkaUtility {
 		if(null == client) {
 			client = AdminClient.create(getAdminConfig());
 		}
-		//Returning the clinet
+		//Returning the clinet   
 		return client;
 	}
 	
